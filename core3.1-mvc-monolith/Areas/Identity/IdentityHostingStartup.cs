1cb004075baf2ae8acb5ae93708a62b5832c28d4
Ron Mobashar <ronmobashar@gmail.com>
2020-04-24 13:32:18 +1000

    Added identity, bootstrap and jquery

---
+++ core3.1-mvc-monolith/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using core3._1_mvc_monolith.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(core3._1_mvc_monolith.Areas.Identity.IdentityHostingStartup))]
+namespace core3._1_mvc_monolith.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
