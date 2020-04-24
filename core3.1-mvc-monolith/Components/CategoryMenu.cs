3d90d3c3a2360ace6996babfb6edc306b663f589
Ron Mobashar <ronmobashar@gmail.com>
2020-04-24 14:40:52 +1000

    Added View Components

---
+++ core3.1-mvc-monolith/Components/CategoryMenu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using core3._1_mvc_monolith.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace core3._1_mvc_monolith.Components
+{
+    public class CategoryMenu : ViewComponent
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        public CategoryMenu(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = _categoryRepository.AllCategories.OrderBy(c => c.CategoryName);
+            return View(categories);
+        }
+    }
+}
---
+++ core3.1-mvc-monolith/Components/ShoppingCartSummary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using core3._1_mvc_monolith.Models;
+using core3._1_mvc_monolith.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace core3._1_mvc_monolith.Components
+{
+    public class ShoppingCartSummary: ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+
+            };
+            return View(shoppingCartViewModel);
+        }
+    }
+}
